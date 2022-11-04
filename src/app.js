518166465b59a3c6a9d8546c5672cbad4148cc08
alenleonidas <alanleonidas.leo@gmail.com>
2022-11-03 23:37:05 -0300

    Adicionando cors

--- src/app.js
+++ src/app.js
@@ -1,10 +1,8 @@
 const express = require("express");
 const router = require("./router");
 const app = express();
-const cors = require("cors");
 
 app.use(express.json());
 app.use(router);
-app.use(cors);
 
 module.exports = app;
--- src/router.js
+++ src/router.js
@@ -1,7 +1,9 @@
 const express = require("express");
+const cors = require("cors");
 const categoriaController = require("./controllers/categoriaController");
 const categoriaMiddleware = require("./middlewares/categoriaMiddleware");
 const router = express.Router();
+router.use(cors);
 
 
 router.get("/", (request, response) => response.status(200).send("Serviço ativo"));
