43177fe0179a40e3f84070b2fed28d81073098b2
alenleonidas <alanleonidas.leo@gmail.com>
2022-11-04 00:26:19 -0300

    Adicionando cors

--- src/app.js
+++ src/app.js
@@ -1,8 +1,20 @@
 const express = require("express");
 const router = require("./router");
+const cors = require("cors");
 const app = express();
 
 app.use(express.json());
+
+app.use((req, res, next) => {
+    console.log("acessou o middlewares");
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "*");    
+    res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
+    app.use(cors());
+    next();
+});
+
+
 app.use(router);
 
 module.exports = app;
