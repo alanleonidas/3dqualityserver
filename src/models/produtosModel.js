de6f3779791d9ee1bfff828d813d61a6e4012dc7
alenleonidas <alanleonidas.leo@gmail.com>
2022-11-20 11:16:30 -0300

    Correção de Bug no updte

--- src/models/produtosModel.js
+++ src/models/produtosModel.js
@@ -41,8 +41,8 @@ const updateProduto = async (id, produto) =>{
         , dataPromocaoInicio, dataPromocaoFim, valorPromocao, inativo, idCategoria, fotos} = produto;
     const dateUTC = new Date(Date.now()).toUTCString();
     const [updateProduto] = await connection.execute("UPDATE PRODUTOS SET PR_DESCRICAO = ?, PR_OBSERVACAO = ?, PR_DESCRICAORESUMIDA = ?, "+
-        "PR_VALORVENDA = ?, PR_PROMOCAO = ?, PR_DATAPROMOCAOINICIO = ?, PR_DATAPROMOCAOFIM = ?, PR_VALORPROMOCAO = ?, PR_INATIVO = ? "+
-        "  PR_FOTOS = ?, PR_IDCATEGORIA = ?, update_at = ? WHERE ID = ?", [descricao, observacao, descricaoResumida, valorVenda, promocao, dataPromocaoInicio, dataPromocaoFim, valorPromocao, inativo, fotos, idCategoria,  dateUTC, id]);
+        "PR_VALORVENDA = ?, PR_PROMOCAO = ?, PR_DATAPROMOCAOINICIO = ?, PR_DATAPROMOCAOFIM = ?, PR_VALORPROMOCAO = ?, PR_INATIVO = ?, "+
+        "PR_FOTOS = ?, PR_IDCATEGORIA = ?, update_at = ? WHERE ID = ?", [descricao, observacao, descricaoResumida, valorVenda, promocao, dataPromocaoInicio, dataPromocaoFim, valorPromocao, inativo, fotos, idCategoria,  dateUTC, id]);
 
     return updateProduto;
 };
