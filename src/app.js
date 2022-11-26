1ebadaa30524edcbf9a26ab7575beb5dc62f0ab6
alenleonidas <alanleonidas.leo@gmail.com>
2022-11-26 13:12:43 -0300

    Adicionado https

--- src/app.js
+++ src/app.js
@@ -1,7 +1,20 @@
 const express = require("express");
+const https = require("https");
+const fs = require('fs');
 const router = require("./router");
 const cors = require("cors");
+
+
+var key = fs.readFileSync(__dirname + "/../certs/selfsigned.key");
+var cert = fs.readFileSync(__dirname + "/../certs/selfsigned.crt");
+var options = {
+    key: key,
+    cert: cert
+};
+
+
 const app = express();
+
 
 app.use(express.json());
 
@@ -17,4 +30,7 @@ app.use((req, res, next) => {
 
 app.use(router);
 
-module.exports = app;
+var server = https.createServer(options, app);
+
+
+module.exports = server;
