a0cba6d2a733321509cae16066ad0108c6ad1d93
alenleonidas <alanleonidas.leo@gmail.com>
2022-11-27 13:50:40 -0300

    Criação do certificado

--- src/server.js
+++ src/server.js
@@ -3,7 +3,9 @@ require("dotenv").config();
 
 // eslint-disable-next-line no-undef
 const PORT = process.env.PORT || 3001;
+const MYSQL_HOST = process.env.MYSQL_HOST;
+
 
 app.listen(PORT, ()=>{
-    console.log(`Rodando em HTTPS na porta  ${PORT}`);
+    console.log(`Rodando em HTTPS na porta  ${PORT} host ${MYSQL_HOST}`);
 });
