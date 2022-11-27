5e7317c3e7510cfc98a32922ce1a071d83dccda5
alenleonidas <alanleonidas.leo@gmail.com>
2022-11-27 13:57:42 -0300

    Commit para subir no ec2

--- src/server.js
+++ src/server.js
@@ -1,11 +1,11 @@
 const app = require("./app");
 require("dotenv").config();
 
+console.log(process.env);
+
 // eslint-disable-next-line no-undef
 const PORT = process.env.PORT || 3001;
-const MYSQL_HOST = process.env.MYSQL_HOST;
-
 
 app.listen(PORT, ()=>{
-    console.log(`Rodando em HTTPS na porta  ${PORT} host ${MYSQL_HOST}`);
+    console.log(`Rodando na porta  ${PORT} `);
 });
