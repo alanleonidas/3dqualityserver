8194919fe1d57801e5cda4aa8a1b49c94d11999c
alenleonidas <alanleonidas.leo@gmail.com>
2022-11-27 14:36:20 -0300

    ecosystem.conf

---
+++ ecosystem.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+    apps: [
+      {
+        name: "Backend 3DQuality",
+        script: ".src/server.js",
+        exp_backoff_restart_delay: 100,
+        env: {
+            NODE_ENV: "prod",
+            PORT: "3001",
+            MYSQL_HOST:"108.167.188.28",
+            MYSQL_USER:"hg3dqu67_root",
+            MYSQL_PASSWORD:"A813200DLT!@rg",
+            MYSQL_DATABASE:"hg3dqu67_site",
+        },
+      },
+    ],
+  };
+  
