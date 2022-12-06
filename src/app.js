de03b7d8667cf7ad67b7a9201fa78f0e6a1f088d
alenleonidas <alanleonidas.leo@gmail.com>
2022-12-05 22:37:25 -0300

    Teste de Cors hospedagem

--- src/app.js
+++ src/app.js
@@ -1,6 +1,4 @@
 const express = require("express");
-// const https = require("https");
-// const fs = require("fs");
 const router = require("./router");
 const cors = require("cors");
 
@@ -9,7 +7,7 @@ const app = express();
 app.use(express.json());
 
 app.use((req, res, next) => {
-    console.log("acessou o middlewares");
+    console.log("Acessou para Liberar o CORS");
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "*");    
     res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
@@ -17,13 +15,6 @@ app.use((req, res, next) => {
     next();
 });
 
-
 app.use(router);
 
-// var server = https.createServer({
-//     cert: fs.readFileSync("src/certs/certificate.crt"),
-//     key: fs.readFileSync("src/certs/certificate.key")
-// }, app);
-
 module.exports = app;
-// module.exports = server;
--- src/router.js
+++ src/router.js
@@ -1,20 +1,22 @@
 const express = require("express");
+const cors = require("cors");
+
 const categoriaController = require("./controllers/categoriaController");
 const categoriaMiddleware = require("./middlewares/categoriaMiddleware");
 const produtoController = require("./controllers/produtosController");
 const produtoMiddleware = require("./middlewares/produtosMiddleware");
 const router = express.Router();
 
-router.get("/", (request, response) => response.status(200).send("Serviço ativo"));
+router.get("/", cors(), (request, response) => response.status(200).send("Serviço ativo"));
 
-router.get("/categoria", categoriaController.getAll);
+router.get("/categoria", cors(), categoriaController.getAll);
 router.post("/categoria", categoriaMiddleware.validateBody, categoriaController.createCategoria);
 router.delete("/categoria/:id", categoriaController.deleteCategoria);
 router.put("/categoria/:id", categoriaMiddleware.validateBody, categoriaController.updateCategoria);
 
 
-router.get("/produto", produtoController.getAll);
-router.get("/produto/:id", produtoController.getByCategoriaAll);
+router.get("/produto", cors(), produtoController.getAll);
+router.get("/produto/:id", cors(), produtoController.getByCategoriaAll);
 router.post("/produto", produtoMiddleware.validateBody, produtoController.createProduto);
 router.delete("/produto/:id", produtoController.deleteProduto);
 router.put("/produto/:id", produtoMiddleware.validateBody, produtoController.updateProduto);
