3f1b593e4ae0da51ac37306cfbaa80a89c67249f
alenleonidas <alanleonidas.leo@gmail.com>
2022-12-07 07:34:08 -0300

    Cors

--- src/app.js
+++ src/app.js
@@ -8,9 +8,9 @@ app.use(express.json());
 
 app.use((req, res, next) => {
     console.log("Acessou para Liberar o CORS");
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "*");    
-    res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
+    // res.header("Access-Control-Allow-Origin", "*");
+    // res.header("Access-Control-Allow-Headers", "*");    
+    // res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
     
     app.use(cors({
         origin: [
