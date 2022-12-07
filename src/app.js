0c7a7561d22899111600bf7f821b7d2012227dbd
alanleonidas <alanleonidas.leo@gmail.com>
2022-12-07 10:08:45 -0300

    Cors

--- src/app.js
+++ src/app.js
@@ -6,21 +6,22 @@ const app = express();
 
 app.use(express.json());
 
+app.use(cors({
+    origin: [
+        "https://api.lttech.com.br",
+        "https://www.api.lttech.com.br",
+        "https://3dqualitybr.com.br",
+        "https://3dqualitybr.com.br/",
+        "https://www.3dqualitybr.com.br",
+    ],
+}),);
+
 app.use((req, res, next) => {
     console.log("Acessou para Liberar o CORS Novo método");
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "*");    
     res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
     
-    app.use(cors({
-        origin: [
-            "https://api.lttech.com.br",
-            "https://www.api.lttech.com.br",
-            "https://3dqualitybr.com.br",
-            "https://3dqualitybr.com.br/",
-            "https://www.3dqualitybr.com.br",
-        ],
-    }),);
     next();
 });
 
