a2d79a975b174f8dc4b818a63f70193e41272c4c
alenleonidas <alanleonidas.leo@gmail.com>
2022-12-07 23:51:36 -0300

    Cors

--- src/app.js
+++ src/app.js
@@ -1,19 +1,19 @@
 const express = require("express");
 const router = require("./router");
-// const cors = require("cors");
+const cors = require("cors");
 
 const app = express();
 
 app.use(express.json());
 
-// app.use(cors({
-//     origin: [
-//         "https://api.lttech.com.br",
-//         "https://www.api.lttech.com.br",
-//         "https://3dqualitybr.com.br",
-//         "https://www.3dqualitybr.com.br",
-//     ],
-// }),);
+app.use(cors({
+    origin: [
+        "https://api.lttech.com.br",
+        "https://www.api.lttech.com.br",
+        "https://3dqualitybr.com.br",
+        "https://www.3dqualitybr.com.br",
+    ],
+}),);
 
 
 // app.use(
@@ -23,23 +23,23 @@ app.use(express.json());
 //     }),
 // );
 
-app.use((req, res, next) => {
-    console.log("Acessou para Liberar o CORS Novo método");
-    res.header("Access-Control-Allow-Credentials", "true");
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "*");    
-    if (req.method==="OPTIONS") {
-        res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
-        return res.status(200).send({});
-    }
-    // app.use(
-    //     cors({
-    //         credentials: true,
-    //         origin: "*"
-    //     }),
-    // );   
-    next();
-});
+// app.use((req, res, next) => {
+//     console.log("Acessou para Liberar o CORS Novo método");
+//     res.header("Access-Control-Allow-Credentials", "true");
+//     res.header("Access-Control-Allow-Origin", "*");
+//     res.header("Access-Control-Allow-Headers", "*");    
+//     if (req.method==="OPTIONS") {
+//         res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
+//         return res.status(200).send({});
+//     }
+//     // app.use(
+//     //     cors({
+//     //         credentials: true,
+//     //         origin: "*"
+//     //     }),
+//     // );   
+//     next();
+// });
 
 app.use(router);
 
