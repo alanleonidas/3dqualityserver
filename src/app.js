91c3c698df7783772a8f62598ef49c4c4e93df65
alenleonidas <alanleonidas.leo@gmail.com>
2022-12-07 22:26:20 -0300

    Cors

--- src/app.js
+++ src/app.js
@@ -36,6 +36,7 @@ app.use((req, res, next) => {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "*");    
     res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
+    app.use(cors());
     
     next();
 });
